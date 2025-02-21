515f1f8109d357ee0f5d4017912ea33e75731844
jaygro <james.grothe@gmail.com>
2025-02-21 11:17:51 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -387,6 +387,7 @@ function generateICS(tasks) {
       'January': '01', 'February': '02', 'March': '03', 'April': '04', 'May': '05', 'June': '06',
       'July': '07', 'August': '08', 'September': '09', 'October': '10', 'November': '11', 'December': '12'
     };
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     const now = new Date().toISOString().replace(/[-:]/g, '').split('.')[0] + 'Z';
     const year = new Date().getFullYear();
